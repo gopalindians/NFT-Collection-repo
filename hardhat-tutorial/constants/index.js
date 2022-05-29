b1c97ba8a197261f96b97a3fcb3cb9e22109f332
Go <gopal@cublocks.com>
2022-05-30 00:07:29 +0530

    init commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x291d33b4D55fE03C0974FC924bfdD62f9B118190";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
